b49e174ed4c73ff9327aaef5f62a8ac0f0fc58e9
saabzaaev <s.ehson.0063@gmail.com>
2023-11-30 15:19:41 +0300

    Objacts

---
+++ main.js
@@ -0,0 +1,52 @@
+// Task-1
+let size =
+{
+  width: 4,
+  length: 2,
+  height: 2,
+};
+function multipling(size)
+{
+  return size.width * size.length * size.height
+}
+console.log(multipling(size));
+
+// Task-2
+function procent(num)
+{
+let dividing = 
+{
+    Needs: 0.5 * num,
+    Wants: 0.3* num,
+    Savings: 0.1 * num
+}
+return dividing
+}
+console.log(procent(10000));
+
+// Task-3
+function helping(str)
+{
+    let femily =
+    {
+    "Darth Vader": "father",
+    "Leia": "sister",
+    "Han": "brother in law",
+    "R2D2": "droid"
+    }
+    return femily["Leia"]
+}
+console.log(helping("Leia"));
+
+// Task-4
+function procent(num)
+{
+let dividing = 
+{
+    Needs: 0.5 * num,
+    Wants: 0.3* num,
+    Savings: 0.1 * num
+}
+return dividing
+}
+console.log(procent(10000));
